e20f9a52be9c2aed7e874d8879f4c60d6f102793
Khalida <xalide.haqverdiyeva@gmail.com>
2025-02-10 19:41:53 +0400

    bug-fix: change file name

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { AuthRegister, AuthLogin, ForgotPassword, resetPassword } = require("../controllers/UserController");
+const { AuthRegister, AuthLogin, ForgotPassword, resetPassword } = require("../controllers/userController");
 
 const router = express.Router();
 
